ab076faa38358eedb459899adb564bd6e17a65ef
nall3n <b_970118@hotmail.com>
2021-09-23 22:54:39 +0200

    Initial commit for refactoring

--- world.py
+++ world.py
@@ -1,12 +1,30 @@
-from game import Room, Item
+
+class Room():
+    def __init__(self) -> None:
+        pass
+
+    def get_info():
+        pass
+
+    def run_event():
+        pass
+
+    def break_stuf():
+        pass ## Dunno kan vara kul ingen aning:::
 
 
-knife = Item('knife', 'A pointy looking thing. Might be good att stabbing things', 3, 2)
-
-world = {}
-
-world['entrance'] = Room('entrance', 'You stand in the entrance of a big house. To the north is a door', ['n'], {'n':'ballroom'}, [knife] ) 
-world['ballroom'] = Room('ballroom', 'You walk in to a big ballroom. In the middle of the room in the floor lies a blänkande svärd! \nThere is a door to the west, east and sout', ['s'], {'s':'entrance'}, None)
+class Item():
+    def __init__(self) -> None:
+        pass
 
 
 
+world_matrix = 5*[5*[Room]]
+
+main_Room = Room()
+
+
+world_matrix[0][0] = main_Room
+
+
+print (world_matrix)
